46ade3c8331f719a2fc2f47a3dccdee9f6c765b9
Eric Hongmoungkhoune <ehmk90@gmail.com>
2021-05-06 16:22:57 -0400

    Implemented a mouse over effect that behaves similar to a real etch-a-sketch

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,12 +1,17 @@
 let gridContainer = document.querySelector('#grid-container');
+let gridSquares = document.querySelectorAll('.grid-square');
 
 function generateGrid() {
     for (let i = 0; i <= 256; i++) {
         let gridSquare = document.createElement('div');
         gridSquare.setAttribute('class', 'grid-square');
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.classList.add('blue');
+        });
         gridContainer.appendChild(gridSquare);
-        console.log(i);
     }
 }
+
+console.log(gridSquares);
 
 generateGrid();
