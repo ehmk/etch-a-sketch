9182cf21f548526d0b41f690a0792c5edd4228ae
Eric Hongmoungkhoune <ehmk90@gmail.com>
2021-05-06 18:53:56 -0400

    Added reset functionality and also a way to generate a new grid of up to, but not over, 100 x 100

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,10 +1,19 @@
+
 let gridContainer = document.querySelector('#grid-container');
-let gridSquares = document.querySelectorAll('.grid-square');
+generateGrid();
+
+let gridSquares = gridContainer.querySelectorAll('.grid-square');
+let gridSquaresArray = Array.from(gridSquares);
+let resetButton = document.querySelector('#reset-grid-btn');
+
+resetButton.addEventListener('click', () => {
+    generateNewGrid();
+});
 
 function generateGrid() {
     for (let i = 0; i <= 256; i++) {
         let gridSquare = document.createElement('div');
-        gridSquare.setAttribute('class', 'grid-square');
+        gridSquare.classList.add('grid-square');
         gridSquare.addEventListener('mouseover', () => {
             gridSquare.classList.add('blue');
         });
@@ -12,6 +21,45 @@ function generateGrid() {
     }
 }
 
-console.log(gridSquares);
+function generateNewGrid() {
+    let height = window.prompt('Generate a new canvas. Height: ', 100);
+    let width = window.prompt('Width: ', 100);
 
-generateGrid();
+    if (height > 100 || width > 100) {
+        alert('100 is the limit.');
+    }
+
+    resetGrid();
+    let area = height * width;
+
+    for (let i = 0; i < height; i++) {
+        gridContainer.style['grid-template-rows'] += " 1fr "
+    }
+    for (let i = 0; i < width; i++) {
+        gridContainer.style['grid-template-columns'] += " 1fr ";
+    }
+    for (let i = 0; i < area; i++) {
+        let gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.classList.add('blue');
+        });
+        gridContainer.appendChild(gridSquare);
+    }
+}
+
+function resetGrid() {
+    let gridSquaresArray = Array.from(gridSquares);
+    for (let i = 0; i < gridSquaresArray.length; i++) {
+        gridSquaresArray[i].classList.remove('blue');
+    }
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    gridContainer.style['grid-template-columns'] = '';
+    gridContainer.style['grid-template-rows'] = '';
+}
+
+
+
